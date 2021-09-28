17b02a44aa89d6c019eee309eac76edc2ecec3af
Adityachess <adityadone9398@gmail.com>
2021-09-28 15:10:34 +0530

    Welcome Message

---
+++ FunctionalPrograms.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.functionalp;
+
+public class FunctionalPrograms {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to functional Programms");
+
+	}
+
+}
