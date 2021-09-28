e28a7184438446a5317d3e9f90c4823ab147ae5e
Adityachess <adityadone9398@gmail.com>
2021-09-28 15:16:24 +0530

    ADD:Two Dimensional Array

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -1,9 +1,39 @@
 package com.bridgelabz.functionalp;
 
+import java.util.Scanner;
+
 public class FunctionalPrograms {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to functional Programms");
+
+		System.out.print("Enter 2D array size : ");
+
+		Scanner sc = new Scanner(System.in);
+
+		int rows = sc.nextInt();
+		int columns = sc.nextInt();
+
+		System.out.println("Enter array elements : ");
+
+		int twoD[][] = new int[rows][columns];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				twoD[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.print(" Data you entered : ");
+
+		for (int[] x : twoD) {
+
+			for (int y : x) {
+
+				System.out.print(y + "        ");
+			}
+
+			System.out.println();
+		}
 
 	}
 
